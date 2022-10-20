49302da3b71df6f3394fb6f3e8912102acc09d48
Luiz <71795494+Luizizizz@users.noreply.github.com>
2022-10-20 01:06:23 +0000

    prints

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Comentario em python
 
 print("Hello world!")
+
+#Quando quiser guardar uma String
+
+nome = "Luiz Claudio"
+idade = 21
+print(nome)
+print("Minha idade é " + str(idade) + " e me chamo" + nome)
 '''
 
 Comentario em bloco
