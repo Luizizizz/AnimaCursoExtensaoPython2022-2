da19b3798c526d6ac69c925b428bac1d762c67a1
Luiz <71795494+Luizizizz@users.noreply.github.com>
2022-10-20 01:23:00 +0000

    fim da aula 19 de outubro

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ print("Hello world!")
 nome = "Luiz Claudio"
 idade = 21
 print(nome)
-print("Minha idade é " + str(idade) + " e me chamo" + nome)
+
+print("Minha idade é " + str(idade) + " e me chamo " + nome)
+
+print(f"Minha idade é {idade}")
+
+print("Minha idade é {}".format(idade))
+
+print("Meu nome é {} e tenho {} anos.".format(nome, idade))
 '''
 
 Comentario em bloco
